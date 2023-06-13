a1882f9ca52c5069de49cb3964003aa97eb47edc
Jordan Streiff <jordan.streiff@gmail.com>
2023-06-13 18:31:26 -0400

    Updates icon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,20 +13,8 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon.png`}
-        />
-        <link
           rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
+          href={`${withPrefix("/")}img/favicon.ico`}
           sizes="16x16"
         />
 
@@ -50,10 +38,6 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
-        />
       </Helmet>
       <div>{children}</div>
     </div>
